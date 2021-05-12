9cf54d20f02aa91ab65434569025c6111c7aaced
leo <>
2021-05-12 11:12:52 +0800

    修改独角兽配置

--- start.sh
+++ start.sh
@@ -3,7 +3,9 @@
 nginx -c /etc/nginx/nginx.conf
 # start Luna
 # gunicorn --workers=4 --bind=0.0.0.0:5000 wsgi:app
-# 50s 超时时常
-gunicorn --worker-class=gevent --threads=10 -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -b 0.0.0.0:5000 wsgi:app –preload -t 50 &
+# 50s 超时时常 协程启动
+# gunicorn --worker-class=gevent --workers=1 -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -b 0.0.0.0:5000 wsgi:app –preload -t 50 &
+# 多线程启动
+gunicorn --workers=1 --threads=17 -k geventwebsocket.gunicorn.workers.GeventWebSocketWorker -b 0.0.0.0:5000 wsgi:app –preload -t 50 &
 # 开启ssh
 /usr/sbin/sshd -D
